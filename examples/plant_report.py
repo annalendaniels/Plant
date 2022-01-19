45b39193ca78223d3c2e6abfe26e9e639176b286
SamTov <tovey.samuel@gmail.com>
2022-01-19 17:05:40 +0100

    add loading bar.

--- examples/plant_report.py
+++ examples/plant_report.py
@@ -3,6 +3,7 @@ Example script to report plant data every 10 seconds
 """
 import plant
 import time
+from tqdm import tqdm
 
 light_sensor = plant.sensors.light_sensors.BH170()
 temperature_sensor = plant.sensors.temperature_sensors.BMP180()
@@ -10,7 +11,8 @@ temperature_sensor = plant.sensors.temperature_sensors.BMP180()
 example_plant = plant.Plant(sensors=[light_sensor, temperature_sensor])
 
 print("Initializing sensors")
-time.sleep(5)
+for _ in tqdm(range(5), ncols=50):
+    time.sleep(1)
 
 while True:
     data = example_plant.collect_plant_data()  # collect a dict of data
